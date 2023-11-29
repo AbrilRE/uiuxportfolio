fc36811389c3c7864a72c17ee1590f2cd49cf3cc
AbrilRE <abril-re@hotmail.com>
2023-11-28 23:52:41 -0600

    Responsive terminado para mac, tablet, medio y peque;o

--- JS/script.js
+++ JS/script.js
@@ -1,15 +1,46 @@
 
-function openModal(id){
+function openModal(id) {
     document.getElementById(id).style.animation = "growUp 1s 1";
-    setTimeout(function(){
+    setTimeout(function () {
         document.getElementById(id).style.display = 'block';
     }, 500)
 }
 
-function closeModal(idClose){
+function closeModal(idClose) {
     document.getElementById(idClose).style.animation = "growBack 1s 1";
-    setTimeout(function(){
+    setTimeout(function () {
         document.getElementById(idClose).style.display = 'none';
     }, 900)
 
 }
+
+const options = document.getElementsByClassName('.option');
+options.addEventListener("click", function close() {document.getElementById('submenu').style.display = 'none';});
+
+function openSubmenu() {
+    const submenu = document.getElementById('submenu');
+
+    if (submenu.style.display == 'flex') {
+        submenu.style.display = 'none';
+    }
+
+    else {
+        submenu.style.display = 'flex';
+    }
+}
+
+function openMobileMenu(){
+    const menu = document.getElementById('mobilemenu');
+    const options = document.getElementById('mobilemenuoptions');
+
+    if (options.style.display == 'flex') {
+        options.style.display = 'none';
+        menu.style.height = '4vh';
+    }
+
+    else {
+        options.style.display = 'flex';
+        menu.style.height = '25vh';
+    }
+}
+
